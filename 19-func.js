c19549c0b3cc2e5b329f0a7c1adbd6e19922f9bc
Veysel Alan <122131026+veys97@users.noreply.github.com>
2023-09-14 00:56:51 +0300

    Add files via upload

---
+++ 19-func.js
@@ -0,0 +1,15 @@
+// *** Ilk Fonksiyonumuzu Tanimlamak *** // 
+
+function hello() {
+    console.log("Merhaba")
+    helloWorld()
+}
+
+function helloWorld() {
+    console.log("Hello World!")
+}
+
+// fonksiyonların hangi sırayla çağırıldığı önemli 
+
+// helloWorld()
+hello()
