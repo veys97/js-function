2cfced858bfe5a21b9a89f81dc5ba274352ae052
Veysel Alan <122131026+veys97@users.noreply.github.com>
2023-09-14 01:05:12 +0300

    Update 19-func.js

--- 19-func.js
+++ 19-func.js
@@ -10,6 +10,19 @@ function helloWorld() {
 }
 
 // fonksiyonların hangi sırayla çağırıldığı önemli 
+// Hata alabiliriz! (Disarıdan age ve userName bilgisine ihtiyacımız var. Bu yüzden fonksiyomuz düzgün çalışmaz.)
+// function userCheck() {
+//     if (userName && age >= 18) {
+//         info.innerHTML = " Ehliyet alabilirsiniz. " 
+//     }
+//     else if (!userName) {
+//         info.innerHTML = " Kullanici adiniz yok. "
+//     }
+//     else if (!age) {
+//         info.innerHTML = " Yas bilginiz yok veya 18 yasindan kücüksünüz. "
+//     }
+// }
 
-// helloWorld()
+// fonksiyonların hangi sırayla çağırıldığı önemli 
 hello()
+
